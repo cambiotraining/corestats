341d70cf3cdd7b62a2c1fa1dc898eb85027eb397
Martin van Rongen <martin@vanrongen.net>
2022-09-20 16:09:31 +0100

    rebuild to template

---
+++ materials/setup_files/setup.py
@@ -0,0 +1,9 @@
+from plotnine import *
+import pandas as pd
+import pingouin as pg
+from scipy import stats
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+exec(open('setup_files/dgplots_knitr.py').read())
+#from dgplots_knitr import *
+#from dgplots import *
